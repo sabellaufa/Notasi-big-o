279f96175207b203c5aca9226d476c02968f24f0
sabellaufa <161696882+sabellaufa@users.noreply.github.com>
2024-10-01 21:50:27 +0700

    -
    -

---
+++ Tugas.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugas;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Tugas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bilangan = 20;
+        int hasil = 0;
+        
+        for (int i = 0;i <2; i++) {
+            hasil += bilangan;
+        }    
+        System.out.println(hasil+"Hasil dari" + bilangan +"pangkat dua adalah:" +hasil);
+    }
+    
+}
